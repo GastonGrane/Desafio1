6c48cfae35809e528e96cc891e7297823b4fd2e1
Nahuel Lopez <nahuel.lopez@correo.ucu.edu.uy>
2024-08-12 18:53:37 -0300

    Cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -9,7 +9,7 @@ public class Addition
 {
     public static int Add(int a, int b)
     {
-        return a + b;
+        return 0; // Cambio incorrecto
     }
 }
 // Esta clase implementa la operación Division>
